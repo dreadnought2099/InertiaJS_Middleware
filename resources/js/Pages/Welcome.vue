8dc45482ca76ba04d457a527d2a05d007ff51770
dreadnought2099 <magallanesraymart@gmail.com>
2025-01-23 11:54:35 +0800

    add icon & font

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,10 +1,11 @@
 <script setup>
 import { ref } from "vue";
+import "../../css/customFont.css";
 
 const message = ref("This is an emergency!");
 </script>
 
 <template>
-    <h1>Inertia JS</h1>
-    <p>{{ message }}</p>
+    <h1 class="outfit-custom2">Inertia JS</h1>
+    <p class="outfit-custom">{{ message }}</p>
 </template>
